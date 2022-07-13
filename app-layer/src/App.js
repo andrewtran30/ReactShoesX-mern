be1f9db4c3fd3aeda61ca85566ed5912ffe8e921
andrewtran30 <tranandrew30@gmail.com>
2022-07-13 17:50:15 -0400

    refactored code

--- app-layer/src/App.js
+++ app-layer/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">ReactShoesX</a>
-      </header>
-      <main>LKist products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <a href="/">ReactShoesX</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
