0ed169842c63e07b18bdabcddc902efece51a3da
andrewtran30 <tranandrew30@gmail.com>
2022-07-19 14:46:56 -0400

    added bootstrap UI

--- app-layer/src/App.js
+++ app-layer/src/App.js
@@ -2,12 +2,22 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <Link to="/">ReactShoesX</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>ReactShoesX</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
           <Routes>
